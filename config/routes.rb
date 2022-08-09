9c24a0433856b2cf4efcb8a2533646572e20d63b
Thomas Chabanne <tchabanne@gmail.com>
2022-08-09 16:33:45 +0200

    rails new

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,3 @@
-
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
-  get 'ask', to: 'pages#ask'
-  get 'answer', to: 'pages#answer'
-  # Generic syntax:
-  # verb 'path', to: 'controller#action'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
